48ec56e28c130ff96eb78d6567dcdfd4c68c23b4
MAYC 6976 <isayah@fungeapp.com>
2022-09-06 16:10:08 -0700

    Updated js features. Added score keeping, game logic, and ability to reset the game. Highscores are also tracked until a user refreshes their browser.

--- script.js
+++ script.js
@@ -12,27 +12,76 @@ document.querySelector('.guess').value;
 document.querySelector('.guess').value = 23;
 */
 
-//Event listener
+//generate random number.
 
-const secretNumber = 0;
+const getRandomNumber = function () {
+  return Math.floor(Math.random() * 100);
+};
+
+//Secret number generator
+let secretNumber = getRandomNumber();
+console.log(secretNumber);
+
+//Track score
+let score = 20;
+
+//Track highscore
+let highscore = 0;
+
+//Event listener
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  const score = Number(document.querySelector('.score').textContent);
-  console.log(score);
 
   //No input entered
   if (!guess) {
     document.querySelector('.message').textContent = 'No number entered!';
-    //guess too low
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = `Higher than ${guess}`;
-    //guess too high
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = `Lower than ${guess}`;
-  } else if (guess === secretNumber) {
-    document.querySelector(
-      '.message'
-    ).textContent = `${guess} is the correct answer! Your score is: ${score}`;
+  } else if (score > 0) {
+    if (guess < secretNumber) {
+      document.querySelector('.message').textContent = `Higher than ${guess}`;
+      score -= 1;
+      //guess too high
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = `Lower than ${guess}`;
+      score -= 1;
+      //guess is correct
+    } else if (guess === secretNumber) {
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+
+      document.querySelector(
+        '.message'
+      ).textContent = `${guess} is the correct answer!`;
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('body').style.backgroundColor = '#60B347';
+      document.querySelector('.number').style.width = '30rem';
+    }
+    if (score === 0) {
+      document.querySelector('.message').textContent =
+        'You have ran out of guesses. You lose.';
+    }
+  } else if (score === 0) {
+    document.querySelector('.message').textContent =
+      'You\'ve lost. Press "Again!" to play again';
   }
+
+  //update score:
+  document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').style.backgroundColor = '#eee';
+  document.querySelector('.message').textContent =
+    'Game reset. Start Guessing!';
+  document.querySelector('.guess').value = '';
+  secretNumber = getRandomNumber();
+  console.log(secretNumber);
 });
